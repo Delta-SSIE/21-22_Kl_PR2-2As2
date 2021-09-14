c5b4d28ed14368bbd6213a48fbb58fcb837d8a8b
Jan Koupil <jan.koupil@delta-skola.cz>
2021-09-14 09:36:10 +0200

    2. lekce

---
+++ 01-Uvod_01-Prohozeni-promennych/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _01_Uvod_01_Prohozeni_promennych
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            //double quotient = (double)x / y;
+            //Console.WriteLine($"Podíl je {quotient}");
+
+            //temporary variable
+
+            //int temp = x;
+            //x = y;
+            //y = temp;
+
+
+            //destructuring
+            //(x, y) = (y, x);
+
+
+            //odčítání
+            //x = x + y;
+            //y = x - y;
+            //x = x - y;
+
+
+            //xor
+
+            x = x ^ y;
+            y = x ^ y;
+            x = x ^ y;
+
+            Console.WriteLine($"x:{x}, y:{y}");
+
+        }
+    }
+}
