bacc9a718d2b1eaf94173550ac6bd089b8887a66
Jan Koupil <jan.koupil@gmail.com>
2021-09-21 08:31:25 +0200

    21.9. TryParse ukázka

---
+++ 01-Uvod_02-TryParse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01_Uvod_02_TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Napiš celé číslo: ");
+            //string nacteno = Console.ReadLine();
+
+            //int cislo = int.Parse(nacteno);
+            //int cislo = Convert.ToInt32(nacteno);
+
+
+            //int cislo;            
+            //if (int.TryParse(nacteno, out cislo)) 
+            //{  
+            //    Console.WriteLine($"Napsal jsi {cislo}.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("To není celé číslo.");
+            //}
+
+            Console.WriteLine("Napiš celé číslo: ");
+            string nacteno = Console.ReadLine();
+            int cislo;
+
+            while (!int.TryParse(nacteno, out cislo))
+            {
+                Console.WriteLine("Má to být celé číslo, truhlíku!");
+                nacteno = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Napsal jsi {cislo}.");
+
+        }
+    }
+}
