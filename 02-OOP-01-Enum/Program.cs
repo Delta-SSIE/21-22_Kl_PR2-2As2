3fc780dae3f26db6b46399bd45a95b770e344772
Jan Koupil <jan.koupil@gmail.com>
2021-10-05 09:32:18 +0200

    5. 10. 2021 - Enum

---
+++ 02-OOP-01-Enum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02_OOP_01_Enum
+{
+    enum Obdobi : byte { Jaro, Leto, Podzim, Zima, Fujtajbl = Podzim };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obdobi chladne = Obdobi.Jaro;
+            Console.WriteLine(chladne);
+
+            Obdobi teple = chladne + 1;
+            Console.WriteLine(teple);
+
+            Obdobi dalsi = teple + 2;
+            Console.WriteLine(dalsi);
+
+            dalsi++;
+            Console.WriteLine(dalsi);
+
+            Obdobi barevne = (Obdobi)2;
+            Console.WriteLine(barevne);
+
+            //char pismeno = (char)('a' + 3);
+
+            dalsi = (Obdobi)255;
+            dalsi++;
+            Console.WriteLine(dalsi);
+
+            
+        }
+    }
+}
