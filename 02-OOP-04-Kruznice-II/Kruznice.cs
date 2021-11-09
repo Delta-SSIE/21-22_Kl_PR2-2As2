71832b5e41cba6a69cd7bf5264b5f4d408806adf
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-09 08:44:17 +0100

    lekce 2021-11-02 OOP - konstruktory, gettery

---
+++ 02-OOP-04-Kruznice-II/Kruznice.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOP_04_Kruznice_II
+{
+    class Kruznice
+    {
+        private double _polomer;
+
+        public double GetPolomer()
+        {
+            return _polomer;
+        }
+        public void SetPolomer(double hodnota)
+        {
+            if (hodnota < 0)
+            {
+                //Console.WriteLine("Neplatná hodnota poloměru");
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                _polomer = hodnota;
+            }
+        } 
+
+        public Kruznice(double polomer)
+        {
+            SetPolomer(polomer);
+        }
+        public Kruznice()
+        {
+            SetPolomer(0);
+        }
+
+        public double SpoctiObvod()
+        {
+            return 2 * Math.PI * _polomer;
+        }
+
+        public double SpoctiObsah()
+        {
+            return Math.PI * _polomer * _polomer;
+        }
+
+
+    }
+}
---
+++ 02-OOP-04-Kruznice-II/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02_OOP_04_Kruznice_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try 
+            //{ 
+            //    int.Parse(Console.ReadLine());
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Co to píšeš, truhlíku!");
+            //}
+            Kruznice o = new Kruznice(-1);
+
+            Kruznice k = new Kruznice();
+           
+            Console.WriteLine($"Kružnice o poloměru {k.GetPolomer()} cm má obvod {k.SpoctiObvod():0.00} cm a obsah {k.SpoctiObsah():0.00} cm^2.");
+
+            Kruznice l = new Kruznice(1);
+
+            Console.WriteLine($"Kružnice o poloměru {l.GetPolomer()} cm má obvod {l.SpoctiObvod():0.00} cm a obsah {k.SpoctiObsah():0.00} cm^2.");
+        }
+
+        //static int Secti(int a, int b)
+        //{
+        //    return a + b;
+        //}
+
+        //static int Secti(int a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+
+        //static double Secti(double a, double b)
+        //{
+        //    return a + b;
+        //}
+
+    }
+}
