fd76a7f7c853cc2e19d69e665299d4824182f28b
Jan Koupil <jan.koupil@gmail.com>
2021-11-09 13:23:39 +0100

    2021-11-09 Vlastnosti, reseni replit predchozich uloh

---
+++ 02-OOP-05-Kruznice-III/Kruznice.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_OOP_05_Kruznice_III
+{
+    class Kruznice
+    {
+        private double _polomer;
+        public double Polomer
+        {
+            get
+            {
+                return _polomer;
+            }
+            //get => _polomer; //jiný zápis getteru
+            private set
+            {
+                if (value < 0)
+                {
+                    //Console.WriteLine("Neplatná hodnota poloměru");
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    _polomer = value;
+                }
+            }
+        }
+
+        public Kruznice(double polomer)
+        {
+            Polomer = polomer;
+        }
+        public Kruznice()
+        {
+            Polomer = 0;
+        }
+
+        public double Obvod
+        {
+            get
+            {
+                return 2 * Math.PI * _polomer;
+            }
+        }
+
+        public double Obsah
+        {
+            get
+            {
+                return Math.PI * _polomer * _polomer;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ 02-OOP-05-Kruznice-III/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02_OOP_05_Kruznice_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            { 
+                Kruznice o = new Kruznice(-1);
+            }
+            catch
+            {
+                Console.WriteLine("Nesmyslná hodnota poloměru.");
+            }
+
+
+
+            Kruznice k = new Kruznice();
+
+            Console.WriteLine($"Kružnice o poloměru {k.Polomer} cm má obvod {k.SpoctiObvod():0.00} cm a obsah {k.Obsah:0.00} cm^2.");
+
+            Kruznice l = new Kruznice(1);
+            //l.Polomer = 4; //nelze, pokud máme private set
+
+            Console.WriteLine($"Kružnice o poloměru {l.Polomer} cm má obvod {l.SpoctiObvod():0.00} cm a obsah {k.Obsah:0.00} cm^2.");
+
+            // l.Obsah = 6; nelze - není setter, je readonly
+        }
+    }
+}
