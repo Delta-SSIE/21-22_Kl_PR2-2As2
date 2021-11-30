8d141f8cfab6c1d8df7613e9629a0bbe67b3a478
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-30 11:17:07 +0100

    2021-11-30 - WPF úvod

---
+++ 03-WPF-01-Hello-world/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_WPF_01_Hello_world
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            HelloLbl.Foreground = Brushes.Red;
+        }
+
+        private void HelloLbl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            HelloLbl.Foreground = Brushes.Black;
+        }
+
+        private void HelloLbl_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            HelloLbl.Foreground = Brushes.Blue;
+        }
+    }
+}
---
+++ XX-file-read-write-demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace XX_file_read_write_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = @"d:\temp\demotext.txt";
+            ///* najednou */            
+            //string text = System.IO.File.ReadAllText(filename);
+            //Console.WriteLine(text);
+
+            /* po řádcích */
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+    }
+}
