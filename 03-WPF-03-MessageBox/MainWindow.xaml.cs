3d0d67e670e7f3290c6d7bb5ef09368a6500099d
Jan Koupil <jan.koupil@gmail.com>
2021-12-14 09:34:43 +0100

    2021-12-14 - WPF messageBox, checkbox

---
+++ 03-WPF-03-MessageBox/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_WPF_03_MessageBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowPopupBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Pozor, smažu vše na disku C:\\!", "Varování", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+
+            switch (result)
+            {
+                case MessageBoxResult.Cancel:
+                    OutputTB.Text = "Nic";
+                    break;
+
+                case MessageBoxResult.Yes:
+                    OutputTB.Text = "Mažu";
+                    break;
+
+                case MessageBoxResult.No:
+                    OutputTB.Text = "Nemažu";
+                    break;
+            }
+        }
+    }
+}
---
+++ 03-WPF-04-O-deset-vic/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_WPF_04_O_deset_vic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //InputTB.RaiseEvent(new TextChangedEventArgs( );
+            InputTB_TextChanged(null, null);
+            {
+                //AddBtn.RaiseEvent(new RoutedEventArgs(System.Windows.Controls.Primitives.ButtonBase.ClickEvent));
+            }
+        }
+
+
+
+        private void InputTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (OutputTB == null)
+                return;
+
+            if (double.TryParse(InputTB.Text, out double number))
+            {
+                number += 10;
+                OutputTB.Text = number.ToString();
+
+                InputTB.BorderBrush = Brushes.Black;
+            }
+            else
+            {
+                OutputTB.Text = "N/A";
+                InputTB.BorderBrush = Brushes.Red;
+            }
+        }
+    }
+}
