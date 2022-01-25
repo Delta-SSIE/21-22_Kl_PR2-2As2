01907c7ba8ac2d6c6cb47f295ab927ea6d17e3aa
Jan Koupil <jan.koupil@gmail.com>
2022-01-25 11:21:45 +0100

    2022-01-25 Calculator - první tlačítka

--- 03-WPF-11-Calculator/MainWindow.xaml.cs
+++ 03-WPF-11-Calculator/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace _03_WPF_11_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        //proměnné: 
+
+        //aktuální číslo
+        private string currentText = "0";
+        //minulé číslo
+        private double lastValue = 0;
+        //zapamatovaná operace
+        private SimpleMath.Operation operation;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void NumberBtn_Click(object sender, RoutedEventArgs e)
+        {            
+            Button clickedBtn = (Button)sender; //pracuj s proměnnou sender jako by to byl button
+            
+            //vezmi aktuální text v tlačítku
+            string number = clickedBtn.Content.ToString();
+
+            //přilep za současné číslo číslo z tlačítka
+            if (currentText != "0")
+                currentText += number;
+            else
+                currentText = number;
+
+            //zobraz na displeji
+            Render();
+        }
+
+        private void Render()
+        {
+            Display.Text = currentText;
         }
     }
 }
---
+++ 03-WPF-11-Calculator/SimpleMath.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_WPF_11_Calculator
+{
+    static class SimpleMath
+    {
+        public enum Operation { Addition, Subtraction, Multiplication, Division }
+        public static double Addition(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public static double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public static double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public static double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public static double Percent(double num)
+        {
+            return num / 100;
+        }
+
+    }
+}
